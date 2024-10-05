cf3a7c9789f71b72b98e5348f599cd6461770855
Daniel Berry <dberry@sagecomputing.com>
2024-10-05 18:27:37 -0400

    Fix persistent session issues

--- models/session.go
+++ models/session.go
@@ -7,32 +7,25 @@ import (
 )
 
 const SESSKEY = "SESSKEY"
-var cookieStore *sessions.CookieStore
-var session *sessions.Session
 
 
 func GetCookieStore(req *http.Request) *sessions.CookieStore {
-	if cookieStore == nil {
-		sessionKey, err := Config("SESSION", "SESSION_KEY")
-		if err != nil {
-			panic(err)
-		}
-
-		key := []byte(sessionKey)
-		cookieStore = sessions.NewCookieStore(key)
+	sessionKey, err := Config("SESSION", "SESSION_KEY")
+	if err != nil {
+		panic(err)
 	}
+
+	key := []byte(sessionKey)
+	cookieStore := sessions.NewCookieStore(key)
 	return cookieStore
 }
 
 
 func GetSession(req *http.Request) *sessions.Session {
-	if session == nil {
-		store := GetCookieStore(req)
-		var err error
-		session, err = store.Get(req, SESSKEY)
-		if err != nil {
-			panic(err)
-		}
+	store := GetCookieStore(req)
+	session, err := store.Get(req, SESSKEY)
+	if err != nil {
+		panic(err)
 	}
 
 	return session
