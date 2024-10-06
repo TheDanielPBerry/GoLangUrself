86223130bf77c9ab90871ca6eb2a8d71f8740096
Daniel Berry <dberry@sagecomputing.com>
2024-10-06 15:02:18 -0400

    Drop HTMX, preview styling, and better homepage controls

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,24 +10,46 @@ window.addEventListener('load', () => {
 			return;
 		}
 
-		//Clear out focus of all thumbnails
-		let focused = document.getElementsByClassName('thumbnail-focus');
-		if(focused.length > 0) {
-			for(let i=0; i<focused.length; i++) {
-				focused[i].classList.remove('thumbnail-focus');
-			}
-		}
 
 		let thumbnail = target.closest('.thumbnail');
 		if(thumbnail) {
-			thumbnail.classList.add('thumbnail-focus');
-		}
+			let open = thumbnail.getAttribute('focus') == 'focus';
 
-		let rows = document.getElementsByClassName('row-container');
-		for(let i=0; i<rows.length; i++) {
-			if(!rows[i].contains(thumbnail)) {
-				rows[i].querySelector('.preview').innerHTML = '';
+			//Close other open previews
+			let rows = document.getElementsByClassName('row-container');
+			for(let i=0; i<rows.length; i++) {
+				if(open || !rows[i].contains(thumbnail)) {
+					let preview = rows[i].querySelector('.preview');
+					preview.innerHTML = '';
+					preview.classList.add('hide');
+				}
+			}
+
+			//Clear out focus of all thumbnails
+			let focused = document.querySelectorAll('.thumbnail[focus=focus]');
+			if(focused.length > 0) {
+				for(let i=0; i<focused.length; i++) {
+					if(open || focused[i] != thumbnail) {
+						focused[i].removeAttribute('focus');
+					}
+				}
+			}
+
+			if(!open) {
+				thumbnail.setAttribute('focus', 'focus');
+
+				let preview = target.closest('.row-container').querySelector('.preview');
+				//Fetch preview html
+				let videoId = thumbnail.getAttribute('data-video-id');
+				let url = `/v/${videoId}/preview`;
+				fetch(url, {
+					method: "GET",
+				}).then((resp) => resp.text())
+				.then((text) => preview.innerHTML=text);
+				preview.classList.remove('hide');
 			}
 		}
+
+
 	});
 });
