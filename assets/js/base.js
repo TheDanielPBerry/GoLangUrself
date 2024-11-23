39495dcf46e240a20c4067f9f3dae84e993e09ec
Daniel <dpberry@usca.edu>
2024-11-22 21:26:03 -0500

    Fix js error on base js

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', () => {
-	document.getElementById('account-menu').addEventListener('click', (e) => {
-		let target = e.target;
-		target.nextElementSibling.classList.toggle('hide');
-	});
+	let accountMenu = document.getElementById('account-menu');
+	if(accountMenu) {
+		accountMenu.addEventListener('click', (e) => {
+			let target = e.target;
+			target.nextElementSibling.classList.toggle('hide');
+		});
+	}
 });
